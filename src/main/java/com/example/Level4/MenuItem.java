f715ef9b20728aea8439b5822d2743425ef6ee66
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-01-14 15:15:32 +0900

    25.01.14.15:15

---
+++ src/main/java/com/example/Level4/MenuItem.java
@@ -0,0 +1,14 @@
+package com.example.Level4;
+
+public class MenuItem {
+    //MeunItem 필드
+    String foodName;
+    double foodPrice;
+    String foodInfo;
+
+    public MenuItem(String foodName, double foodPrice, String foodInfo) {
+        this.foodName = foodName;
+        this.foodPrice = foodPrice;
+        this.foodInfo = foodInfo;
+    }
+}
